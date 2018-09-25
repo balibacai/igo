9a698e03cee7163a8d5b972aa139a5cf9b4c2878
mistcheng <guangchao007@126.com>
2018-09-26 01:54:55 +0800

    add orm.debug

--- routers/router.go
+++ routers/router.go
@@ -11,15 +11,52 @@ import (
 	"beego/controllers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"github.com/dgrijalva/jwt-go"
+	"fmt"
 )
 
 func init() {
+
+	mySigningKey := []byte(beego.AppConfig.String("jwt.secret"))
+
+	type LoginClaims struct {
+		UserID int64
+		jwt.StandardClaims
+	}
+
+	// filters
+	var auth = func(ctx *context.Context) {
+		tokenString := ctx.Input.Header("token")
+		//fmt.Println("token:" + tokenString)
+
+		if len(tokenString) == 0 && ctx.Request.RequestURI != "/v1/login" {
+			ctx.Redirect(302, "/login")
+		}
+
+		// parse token with claims
+		token, err := jwt.ParseWithClaims(tokenString, &LoginClaims{}, func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			ctx.Redirect(302, "/login")
+
+		}
+
+		// validate & extract token
+		if claims, ok := token.Claims.(*LoginClaims); ok && token.Valid {
+			fmt.Printf("%v %v", claims.UserID, claims.StandardClaims.ExpiresAt)
+		} else {
+			fmt.Println(err)
+			ctx.Redirect(302, "/login")
+		}
+	}
+
+	// routers
 	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
+		beego.NSBefore(auth),
 		beego.NSNamespace("/user",
 			beego.NSInclude(
 				&controllers.UserController{},
