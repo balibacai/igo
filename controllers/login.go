40d2493eefc422daea22fa3bb576a1e8b2aca9b1
mistcheng <guangchao007@126.com>
2018-10-20 01:20:02 +0800

    login via auth server

--- controllers/login.go
+++ controllers/login.go
@@ -2,15 +2,10 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/validation"
-	"github.com/astaxie/beego/logs"
 	"time"
-	"beego/extensions"
-	"github.com/dgrijalva/jwt-go"
-	"beego/filters"
-	"beego/models"
-	"beego/requests"
 	"beego/response"
+	"github.com/astaxie/beego/httplib"
+	"fmt"
 )
 
 // LoginController operations for Login
@@ -30,80 +25,16 @@ func (c *LoginController) URLMapping() {
 // @Success 201 {object} models.Login
 // @Failure 403 body is empty
 // @router / [post]
-func (c *LoginController) Post() {
+func (this *LoginController) Post() {
 
 	var result response.JsonResult
 
-	// login credentials
-	credentials := requests.LoginCredentials{}
-	if err := c.ParseForm(&credentials); err != nil {
-		result = response.JsonResult{Error: 100001, Msg: "error occurs when parsing login form"}
-		logs.Error(&result, err)
-		c.Data["json"] = &result
-		c.ServeJSON()
-		return
-	}
+	req := httplib.Post(fmt.Sprintf("%s/v1/login", beego.AppConfig.String("auth.url")))
+	req.Param("email", this.GetString("email", ""))
+	req.Param("password", this.GetString("password", ""))
+	req.SetTimeout(time.Second / 2, time.Second)
+	req.ToJSON(&result)
 
-	logs.Debug("login from data", credentials)
-
-	// validate
-	valid := validation.Validation{}
-	passed, err := valid.Valid(&credentials)
-	if err != nil {
-		result = response.JsonResult{Error: 100002, Msg: "error occurs when validating login credentials"}
-		logs.Error(&result, err)
-		c.Data["json"] = &result
-		c.ServeJSON()
-		return
-	}
-
-	if !passed {
-		result = response.JsonResult{Error: 100003, Msg: "login credentials invalid"}
-		logs.Error(&result, valid.ErrorsMap)
-		c.Data["json"] = &result
-		c.ServeJSON()
-		return
-	}
-
-	// attempt login
-	now := time.Now()
-	user, err := models.GetUserByCredentials(&credentials)
-
-	if err != nil {
-		result = response.JsonResult{Error: 100005, Msg: "user not match"}
-		logs.Error(&result, err)
-		c.Data["json"] = &result
-		c.ServeJSON()
-		return
-	}
-
-	userID := user.Id
-	// expired after 30 days
-	expiredAt := now.Unix() + 2592000
-
-	tokenString, err := extensions.NewJWTTokenStringWithClaims(filters.LoginClaims{
-		UserID: userID,
-		StandardClaims: jwt.StandardClaims {
-			ExpiresAt: expiredAt,
-			Issuer: "igo",
-		},
-	})
-
-	if err != nil {
-		result = response.JsonResult{Error: 100004, Msg: "error occurs when generating login token"}
-		logs.Error(&result, err)
-		c.Data["json"] = &result
-		c.ServeJSON()
-		return
-	}
-
-	tokenMap := map[string]interface{}{
-		"token": tokenString,
-		"expiredAt": expiredAt,
-	}
-
-	// return token
-	result = response.JsonResult{Error: 0, Data: &tokenMap}
-	c.Data["json"] = &result
-	c.ServeJSON()
+	this.Data["json"] = &result
+	this.ServeJSON()
 }
